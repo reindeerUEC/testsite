2e02b3189ef9998f0a593cfba85188099d5c1d17
reindeerUEC <kanaeruhito12@gmail.com>
2023-12-31 14:20:28 +0900

    2

--- scripts/main.js
+++ scripts/main.js
@@ -1,26 +1,35 @@
 const myHeading = document.querySelector("h1");
 myHeading.textContent = "Hello world!";
-let myVariable;
-let myButton = document.querySelector("button");
-let myHeading = document.querySelector("h1");
 
-myVariable = "Bob";
-document.querySelector("html").addEventListener("click", function () {
+document.querySelector("html").addEventListener("click", () => {
     alert("痛っ! つつかないで!");
 });
+
 const myImage = document.querySelector("img");
+let myButton = document.querySelector("button");
 
 myImage.onclick = () => {
     const mySrc = myImage.getAttribute("src");
-    if (mySrc === "images/firefoxImg2.png") {
-        myImage.setAttribute("src", "images/firefoxImg.png");
-    } else {
+    if (mySrc === "images/firefoxImg.png") {
         myImage.setAttribute("src", "images/firefox.jpg");
+    } else {
+        myImage.setAttribute("src", "images/firefoxImg.png");
     }
 };
+
+
 
 function setUserName() {
     const myName = prompt("あなたの名前を入力してください。");
     localStorage.setItem("name", myName);
-    myHeading.textContent = `Mozilla はかっこいいよ、${myName} さん、Mozilla はかっこいいよ。`;
+    if (!localStorage.getItem("name")) {
+        setUserName();
+    } else {
+        const storedName = localStorage.getItem("name");
+        myHeading.textContent = `Mozilla is cool, ${storedName}`;
+    }
 }
+
+myButton.onclick = () => {
+    setUserName();
+};
